10a6bc1f00b61df3aa3b7aa3ecc1c5131d3a8658
Chris <ccuddohy@gmail.com>
2020-10-30 15:14:28 -0400

    initial commit

---
+++ RkProj/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RkProj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RkProj.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		/// <summary>
+		/// create properties to push to the database 
+		/// </summary>
+		/// <param name="options"></param>
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		/// <summary>
+		/// to create the table. The table type and name match the model  
+		/// </summary>
+		public DbSet<Category> Category { get; set; }
+	}
+}
---
+++ RkProj/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RkProj.Models
+{
+	/// <summary>
+	/// all of the properties of this class will be columns of the database. Category is the table name.
+	/// </summary>
+	public class Category
+	{
+		/// <summary>
+		/// SQL Server column can specified as an identity. An identity column means you do not pass
+		/// the value. It automatically increments and adds new values. 
+		/// </summary>
+		[Key]
+		public int Id { get; set; }
+		
+		public string Name { get; set; }
+		public int DisplayOrder { get; set; }
+	}
+}
